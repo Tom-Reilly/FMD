9d7c241c8e9410415c3905a2e7cdd09b62e45062
Tom-Reilly <thomas.reilly@gov.scot>
2019-11-20 08:32:52 +0000

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
-is_attached <- function(pkg) paste0("package:", pkg) %in% search()
+#is_attached <- function(pkg) paste0("package:", pkg) %in% search()
   
-when_attached <- function(pkg, action) {
-  if(is_attached(pkg)) {action}
-}
+#when_attached <- function(pkg, action) {
+#  if(is_attached(pkg)) {action}
+#}
 
 .onLoad <- function(libname = find.package("FMD"), pkgname = "FMD") {
-when_attached("FMD",odbcConnect())
+odbcConnect()
 }
